16b8c8e3984089358be8f5a745c07fbbd69c0cea
Harry <63407848+harry-mohc@users.noreply.github.com>
2021-06-08 16:22:31 +0100

    feat: shipping and billing address (#6)
    Co-authored-by: Harry Withers <harryw@mohc.net>

---
+++ addresses.go
@@ -0,0 +1,35 @@
+package shopify
+
+// Address is an address
+type Address struct {
+	// Address1 - the street address
+	Address1 string
+	// Address2 - an optional additional field for the street address
+	Address2 string
+	// City - the city, town, or village
+	City string
+	// Company - the company of the person associated with this address
+	Company string
+	// Country - the name of the country
+	Country string
+	// CountryCode - the two-letter code (ISO 3166-1 format) for the country of the address
+	CountryCode string
+	// FirstName - the first name of the person associated with the address
+	FirstName string
+	// LastName - the last name of the person associated with the address
+	LastName string
+	// Latitude - the latitude of the address
+	Latitude string
+	// Longitude - the longitude of the address
+	Longitude string
+	// Name - the full name of the person associated with the address
+	Name string
+	// Phone - the phone number at the address
+	Phone string
+	// Province - the name of the region (for example, province, state, or prefecture) of the address
+	Province string
+	// ProvinceCode - the two-letter abbreviation of the region of the address
+	ProvinceCode string
+	// Zip - the postal code (for example, zip, postcode or Eircode) of the address
+	Zip string
+}
