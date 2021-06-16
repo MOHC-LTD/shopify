ee075338d53705b4f47ecc4fac044c6219a38b5f
Harry <63407848+harry-mohc@users.noreply.github.com>
2021-06-16 10:54:54 +0100

    feat: product type (#12)

---
+++ product-constants.go
@@ -0,0 +1,10 @@
+package shopify
+
+const (
+	// ProductStatusActive is the product status when the product is ready to sell and is available to customers on the online store, sales channels, and apps. By default, existing products are set to active.
+	ProductStatusActive = "active"
+	// ProductStatusArchived is the product status when the product is no longer being sold and isn't available to customers on sales channels and apps.
+	ProductStatusArchived = "archived"
+	// ProductStatusDraft is the product status when the product isn't ready to sell and is unavailable to customers on sales channels and apps. By default, duplicated and unarchived products are set to draft.
+	ProductStatusDraft = "draft"
+)
