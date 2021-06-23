c8c342b76b97958e07c97506dcf5a6cf1b279086
Thomas Richards <thomas@mohc.net>
2021-06-23 17:01:27 +0100

    fix: tax rate doesn't need to be that big

--- tax.go
+++ tax.go
@@ -5,5 +5,5 @@ type Tax struct {
 	// Title is the name of this kind of tax
 	Title string
 	// Rate is the percentage of the cost after discounts that should be charged as tax e.g. 0.2 for a 20% tax rate
-	Rate float64
+	Rate float32
 }
