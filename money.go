5a9e9276743703e59cf08945c1e1622e88056cfa
Thomas Richards <83652444+Thomas-Richards-MoHC@users.noreply.github.com>
2021-06-24 09:31:57 +0100

    feat: add costs to orders (#20)
    * feat: add money
    
    * fix: use bash
    
    * docs: add descriptions of new structs
    
    * fix: tax rate doesn't need to be that big
    
    Co-authored-by: Harry <63407848+harry-mohc@users.noreply.github.com>
    Co-authored-by: Harry Withers <harryw@mohc.net>

---
+++ money.go
@@ -0,0 +1,9 @@
+package shopify
+
+// Money is an amount of money in some currency
+type Money struct {
+	// Amount is the amount of money without the currency unit e.g. "5.00"
+	Amount string
+	// CurrencyCode is the 3-letter code that represents a currency e.g EUR or GBP
+	CurrencyCode string
+}
