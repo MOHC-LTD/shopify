90e7eb2a48a58a0d2d2ae755abf6d21e3dcd22cb
Harry <63407848+harry-mohc@users.noreply.github.com>
2021-06-30 18:22:13 +0100

    feat: price fields (#22)
    * fix: git hooks
    
    * feat: price fields
    
    * docs: typo in comment

--- money.go
+++ money.go
@@ -1,5 +1,13 @@
 package shopify
 
+// PriceSet represents a price in shop and presentment currencies
+type PriceSet struct {
+	// ShopMoney is the price in shop currency
+	ShopMoney Money
+	// PresentmentMoney is the price in presentment currency
+	PresentmentMoney Money
+}
+
 // Money is an amount of money in some currency
 type Money struct {
 	// Amount is the amount of money without the currency unit e.g. "5.00"
--- tax.go
+++ tax.go
@@ -1,8 +1,11 @@
 package shopify
 
-// Tax is a tax
-type Tax struct {
-	// Title is the name of this kind of tax
+// TaxLines is a collection of tax lines
+type TaxLines []TaxLine
+
+// TaxLine is a tax line
+type TaxLine struct {
+	// Title is the name of this kind of tax line
 	Title string
 	// Rate is the percentage of the cost after discounts that should be charged as tax e.g. 0.2 for a 20% tax rate
 	Rate float32
