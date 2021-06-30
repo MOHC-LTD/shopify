920b0539aee7292911217dd17e051cde176a8ff5
Harry Withers <harry@mohc.net>
2021-06-30 18:18:36 +0100

    feat: price fields

--- line-items.go
+++ line-items.go
@@ -7,18 +7,28 @@ type LineItems []LineItem
 type LineItem struct {
 	// ID is the ID of the line item
 	ID int64
-	// Title is the title of the product.
-	Title string
-	// Name is the name of the product variant.
-	Name string
-	// SKU is the item's SKU (stock keeping unit).
-	SKU string
+	// Price is the cost of the item before any discounts or tax are applied, in the store's currency
+	Price string
+	// PriceSet is the price of the line item in shop and presentment currencies.
+	PriceSet PriceSet
+	// ProductID is the ID of the product that the line item belongs to. Can be null if the original product associated with the order is deleted at a later date.
+	ProductID int64
 	// Quantity is the number of items that were purchased.
 	Quantity int
-	// Price is the cost of the item before any discounts or tax are applied, in the store's currency
-	Price Money
-	// Discount is the amount of money discounted from the price, in the store's currency
-	Discount Money
-	// Taxes is all the taxes applied to the item
-	Taxes []Tax
+	// SKU is the item's SKU (stock keeping unit).
+	SKU string
+	// Title is the title of the product.
+	Title string
+	// VariantID is the ID of the product variant.
+	VariantID int64
+	// VariantTitle is the title of the product variant.
+	VariantTitle string
+	// Name is the name of the product variant.
+	Name string
+	// TaxLines is a list of tax line objects, each of which details a tax applied to the item.
+	TaxLines TaxLines
+	// TotalDiscount is the total amount of the discount allocated to the line item in the shop currency.
+	TotalDiscount string
+	// TotalDiscountSet is the total amount allocated to the line item in the presentment currency.
+	TotalDiscountSet PriceSet
 }
--- shipping.go
+++ shipping.go
@@ -1,15 +1,22 @@
 package shopify
 
+// ShippingLines is a collection of shipping lines
+type ShippingLines []ShippingLine
+
 // ShippingLine contains details about the shipping method used.
 type ShippingLine struct {
-	// ID is the ID of the shipping method.
-	ID int64
 	// Code is a reference to the shipping method.
 	Code string
+	// Price is the cost of the shipping method, in the store's currency
+	Price string
+	// PriceSet is the price of the shipping method in shop and presentment currencies.
+	PriceSet PriceSet
+	// DiscountedPrice is the price of the shipping method after line-level discounts have been applied. Doesn't reflect cart-level or order-level discounts.
+	DiscountedPrice string
+	// DiscountedPriceSet is the price of the shipping method in both shop and presentment currencies after line-level discounts have been applied.
+	DiscountedPriceSet PriceSet
+	// ID is the ID of the shipping method.
+	ID int64
 	// Title is the title of the shipping method
 	Title string
-	// Price is the cost of the shipping method, in the store's currency
-	Price Money
-	// Discount is the amount the shipping method is discounted by, in the store's currency
-	Discount Money
 }
