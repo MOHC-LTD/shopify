243a08dac3704a7da11410e648076db94f5fdb5c
Jamie <James.coe@mohc.net>
2021-07-12 11:19:35 +0100

    feat: change src comment

--- images.go
+++ images.go
@@ -9,13 +9,7 @@ type Images []Image
 type Image struct {
 	// CreatedAt is the date and time when the product image was created.
 	CreatedAt time.Time
-	// SRC Specifies the location of the product image.
-	/*
-	   This parameter supports URL filters that you can use to retrieve modified copies of the image.
-	   For example, add _small, to the filename to retrieve a scaled copy of the image at 100 x 100 px
-	   (for example, ipod-nano_small.png), or add _2048x2048 to retrieve a copy of the image constrained
-	   at 2048 x 2048 px resolution (for example, ipod-nano_2048x2048.png).
-	*/
+	//  SRC is the source URL that specifies the location of the image.
 	SRC string
 	// Width is the width of the image in pixels.
 	Width int
