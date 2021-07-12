d462aac2a5ab467eadab036119e5a49eeda5f7fa
Jamie <James.coe@mohc.net>
2021-07-12 15:35:07 +0100

    feat: add images to collections

---
+++ images.go
@@ -0,0 +1,20 @@
+package shopify
+
+import "time"
+
+// Images contains a list of images
+type Images []Image
+
+// Image is a basic image object with the essential content
+type Image struct {
+	// CreatedAt is the date and time when the product image was created.
+	CreatedAt time.Time
+	//  SRC is the source URL that specifies the location of the image.
+	SRC string
+	// Width is the width of the image in pixels.
+	Width int
+	// Height is the height of the image in pixels.
+	Height int
+	// Alt is the alternative text that describes the collection image.
+	Alt string
+}
