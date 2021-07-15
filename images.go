cb38a1d150c2b13a0e9db872c13cd1266e3ce330
Jamie <James.coe@mohc.net>
2021-07-15 11:01:32 +0100

    fix: fix commit tag issues

--- images.go
+++ images.go
@@ -5,15 +5,9 @@ import "time"
 // Images contains a list of images
 type Images []Image
 
-<<<<<<< HEAD
-// Image is a basic image object with the essential content
-type Image struct {
-	// CreatedAt is the date and time when the product image was created.
-=======
 // Image is a image
 type Image struct {
 	// CreatedAt is the time and date when the image was added
->>>>>>> develop
 	CreatedAt time.Time
 	// SRC is the source URL that specifies the location of the image.
 	SRC string
@@ -21,10 +15,6 @@ type Image struct {
 	Width int
 	// Height is the height of the image in pixels.
 	Height int
-<<<<<<< HEAD
-	// Alt is the alternative text that describes the collection image.
-=======
 	// Alt is the alternative text that describes the image.
->>>>>>> develop
 	Alt string
 }
