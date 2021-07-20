c988c07c965f87538424c57967b1401c684d89a7
Jamie <James.coe@mohc.net>
2021-07-20 16:52:00 +0100

    fix: add rule constants for relations

---
+++ rules-constants.go
@@ -0,0 +1,24 @@
+package shopify
+
+const (
+	// RuleRelationNumberGreaterThan is number relation where the column value is greater than the condition.
+	RuleRelationNumberGreaterThan = "greater_than"
+	// RuleRelationNumberLessThan is the number relation where the column value is less than the condition.
+	RuleRelationNumberLessThan = "less_than"
+	// RuleRelationNumberEquals is the number relation where the column value is equal to the condition.
+	RuleRelationNumberEquals = "equals"
+	// RuleRelationNumberNotEquals is the number relation where the column value is not equal to the condition.
+	RuleRelationNumberNotEquals = "not_equals"
+	// RuleRelationtTextEquals is the text relation that checks if the column value is equal to the condition value.
+	RuleRelationtTextEquals = "equals"
+	// RuleRelationTextNotEquals is the text relation that checks if the column value is not equal to the condition value.
+	RuleRelationTextNotEquals = "not_equals"
+	// RuleRelationTextStartWith is the text relation that checks if the column value starts with the condition value.
+	RuleRelationTextStartWith = "starts_with"
+	// RuleRelationTextEndWith is the text relation that checks if the column value ends with the condition value.
+	RuleRelationTextEndWith = "ends_with"
+	// RuleRelationTextContains is the text relation that checks if the column value contains the condition value.
+	RuleRelationTextContains = "contains"
+	// RuleRelationTextNotcontainsis the text relation that checks if the column value does not contain the condition value.
+	RuleRelationTextNotcontains = "not_contains"
+)
--- rules.go
+++ rules.go
@@ -21,18 +21,19 @@ type Rule struct {
 	*/
 	Columnn string
 	// Relation is the relationship between the column choice, and the condition.
-	/*Valid values for number relations:
-	  	- greater_than The column value is greater than the condition.
-	  	- less_than The column value is less than the condition.
-	  	- equals The column value is equal to the condition.
-	  	- not_equals The column value is not equal to the condition.
-	  Valid values for text relations:
-	  	- equals: Checks if the column value is equal to the condition value.
-	  	- not_equals: Checks if the column value is not equal to the condition value.
-	  	- starts_with: Checks if the column value starts with the condition value.
-	  	- ends_with: Checks if the column value ends with the condition value.
-	  	- contains: Checks if the column value contains the condition value.
-	  	- not_contains: Checks if the column value does not contain the condition value.
+	/*
+		Valid values for number relations:
+		- greater_than: The column value is greater than the condition.
+		- less_than: The column value is less than the condition.
+		- equals: The column value is equal to the condition.
+		- not_equals: The column value is not equal to the condition.
+		Valid values for text relations:
+		- equals: Checks if the column value is equal to the condition value.
+		- not_equals: Checks if the column value is not equal to the condition value.
+		- starts_with: Checks if the column value starts with the condition value.
+		- ends_with: Checks if the column value ends with the condition value.
+		- contains: Checks if the column value contains the condition value.
+		- not_contains: Checks if the column value does not contain the condition value.
 	*/
 	Relation string
 	// Condition is the select products for a smart collection using a condition. Values are either strings or numbers, depending on the relation value.
