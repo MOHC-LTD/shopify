d09d87a202270627080432b7232087ac254de828
Thomas Richards <83652444+Thomas-Richards-MoHC@users.noreply.github.com>
2021-08-25 09:12:33 +0100

    docs: improve description
    Co-authored-by: Ianmohc <63408186+Ianmohc@users.noreply.github.com>

--- orders.go
+++ orders.go
@@ -82,7 +82,7 @@ type Order struct {
 	SubtotalPrice string
 	// SubtotalPriceSet is the subtotal of the order in shop and presentment currencies after discounts but before shipping, duties, taxes, and tips.
 	SubtotalPriceSet PriceSet
-	// Tags attached to the order. Tags are additional short descriptors, commonly used for filtering and searching. Each individual tag is limited to 40 characters in length and they are separated by commas.
+	// Tags are tags attached to the order, formatted as a string of comma-separated values. Tags are additional short descriptors, commonly used for filtering and searching. Each individual tag is limited to 40 characters in length.
 	Tags Tags
 	// TotalDiscounts is the total discounts applied to the price of the order in the shop currency.
 	TotalDiscounts string
