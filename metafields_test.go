b18c29476b7bb524351a214ad737cf7eaab1cd21
Ianmohc <ian@mohc.net>
2022-08-22 15:41:03 +0100

    refactor: old style testing because of go version

--- metafields_test.go
+++ metafields_test.go
@@ -2,86 +2,102 @@ package shopify
 
 import (
 	"strconv"
+	"testing"
 
 	"github.com/jaswdr/faker"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 )
 
-// Tests that the correct type comes back when getting a metafield by its key
-var _ = Describe("Checking Metafield types are correct when using the GetByKey method", func() {
-	var fake faker.Faker
+/**
+Given I have a single metafield
+When I ask to get that metafield by its key when its of type boolean
+Then the value is returned is the correct type
+*/
+func TestMetafields_GetByKeyBoolean(t *testing.T) {
+	fake := faker.New()
+	// Expects
+	booleanString := fake.Boolean().BoolString("true", "false")
+	expectedValue, _ := strconv.ParseBool(booleanString)
 
-	BeforeEach(func() {
-		fake = faker.New()
-	})
+	metafield := Metafield{}
+	fake.Struct().Fill(&metafield)
+	metafield.Type = BooleanMetaFieldType
+	metafield.Value = booleanString
+	metafields := Metafields{metafield}
 
-	Describe("Given I have a single metafield", func() {
-		Context("When I ask to get that metafield by its key when its of type boolean", func() {
-			It("Then the value is returned in the correct type", func() {
-				// Expects
-				booleanString := fake.Boolean().BoolString("true", "false")
-				expectedValue, err := strconv.ParseBool(booleanString)
-				Expect(err).To(Not(HaveOccurred()))
+	retValue, err := metafields.GetByKey(metafield.Key)
+	if err != nil {
+		t.Errorf("An error was thrown: %v", err)
+	}
 
-				metafield := Metafield{}
-				fake.Struct().Fill(&metafield)
-				metafield.Type = BooleanMetaFieldType
-				metafield.Value = booleanString
-				metafields := Metafields{metafield}
+	if retValue.(bool) != expectedValue {
+		t.Errorf("Was expecting: %v got %v", retValue.(bool), expectedValue)
+	}
+}
 
-				retValue, err := metafields.GetByKey(metafield.Key)
+/**
+Given I have a single metafield
+When I ask to get that metafield by its key when its of type string
+Then the value is returned is the correct type
+*/
+func TestMetafields_GetByKeyString(t *testing.T) {
+	fake := faker.New()
+	expectedValue := fake.Color().Hex()
 
-				Expect(err).To(Not(HaveOccurred()))
-				Expect(retValue.(bool)).To(Equal(expectedValue))
-			})
-		})
+	metafield := Metafield{}
+	fake.Struct().Fill(&metafield)
+	metafield.Type = ColorMetaFieldType
+	metafield.Value = expectedValue
+	metafields := Metafields{metafield}
 
-		Context("When I ask to get that metafield by its key when its of type string", func() {
-			It("Then the value is returned in the correct type", func() {
-				expectedValue := fake.Color().Hex()
+	retValue, err := metafields.GetByKey(metafield.Key)
+	if err != nil {
+		t.Errorf("An error was thrown: %v", err)
+	}
 
-				metafield := Metafield{}
-				fake.Struct().Fill(&metafield)
-				metafield.Type = ColorMetaFieldType
-				metafield.Value = expectedValue
-				metafields := Metafields{metafield}
+	if retValue.(string) != expectedValue {
+		t.Errorf("Was expecting: %v got %v", retValue.(bool), expectedValue)
+	}
+}
 
-				retValue, err := metafields.GetByKey(metafield.Key)
+/**
+Given I have a single metafield
+When I ask to get that metafield by its key when its of type integer
+Then the value is returned is the correct type
+*/
+func TestMetafields_GetByKeyInteger(t *testing.T) {
+	fake := faker.New()
+	expectedValue := fake.Int64()
 
-				Expect(err).To(Not(HaveOccurred()))
-				Expect(retValue.(string)).To(Equal(expectedValue))
-			})
-		})
+	metafield := Metafield{}
+	fake.Struct().Fill(&metafield)
+	metafield.Type = NumberIntegerMetaFieldType
+	metafield.Value = strconv.FormatInt(expectedValue, 10)
+	metafields := Metafields{metafield}
 
-		Context("When I ask to get that metafield by its key when its of type integer", func() {
-			It("Then the value is returned in the correct type", func() {
-				// expected
-				expectedValue := fake.Int64()
+	retValue, err := metafields.GetByKey(metafield.Key)
+	if err != nil {
+		t.Errorf("An error was thrown: %v", err)
+	}
 
-				metafield := Metafield{}
-				fake.Struct().Fill(&metafield)
-				metafield.Type = NumberIntegerMetaFieldType
-				metafield.Value = strconv.FormatInt(expectedValue, 10)
-				metafields := Metafields{metafield}
+	if retValue.(int64) != expectedValue {
+		t.Errorf("Was expecting: %v got %v", retValue.(bool), expectedValue)
+	}
+}
 
-				retValue, err := metafields.GetByKey(metafield.Key)
+/**
+Given I have a single metafield
+When I ask to get that metafield by an invalid key
+Then the value is returned is the correct type
+*/
+func TestMetafields_GetByKeyInvalid(t *testing.T) {
+	fake := faker.New()
+	metafield := Metafield{}
+	fake.Struct().Fill(&metafield)
+	metafield.Key = ""
+	metafields := Metafields{metafield}
 
-				Expect(err).To(Not(HaveOccurred()))
-				Expect(retValue.(int64)).To(Equal(expectedValue))
-			})
-		})
-
-		Context("When I ask to get that metafield by an invalid key", func() {
-			It("Then the correct error message is returned", func() {
-				metafield := Metafield{}
-				fake.Struct().Fill(&metafield)
-				metafield.Key = ""
-				metafields := Metafields{metafield}
-
-				_, err := metafields.GetByKey("123")
-				Expect(err).To(HaveOccurred())
-			})
-		})
-	})
-})
+	_, err := metafields.GetByKey("123")
+	if err == nil {
+		t.Errorf("An error was thrown: %v", err)
+	}
+}
