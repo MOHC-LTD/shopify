d63d63ac623648feca731ba242f9e12b9604bc11
rob-mohc <robert.giurgiulescu@mohc.net>
2022-09-12 13:47:31 +0100

    feat: added function to get a tag's value by key

--- tags.go
+++ tags.go
@@ -14,3 +14,14 @@ func NewTags(tags []string) Tags {
 func (tags Tags) Split() []string {
 	return strings.Split(string(tags), ", ")
 }
+
+// GetTagValue returns the value of a tag based on the key provided. The separator character is used to separate the key from value key:value
+func GetTagValue(tags []string, key string, separator string) string {
+	for _, tag := range tags {
+		if strings.Contains(tag, key) {
+			return strings.Split(tag, separator)[1]
+		}
+	}
+
+	return ""
+}
