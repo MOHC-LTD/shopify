92fb9944a934ea81ab12885ba0cbb8abe309e50e
Eze Lopes <eze.lopes@mohc.net>
2022-10-17 10:20:04 +0100

    feat: updated address field and create address function

--- customer-addresses.go
+++ customer-addresses.go
@@ -2,6 +2,8 @@ package shopify
 
 // CustomerAddressRepository maintains the customer addresses of a shop.
 type CustomerAddressRepository interface {
+	// Create creates a new address against the specified customer
+	Create(id int64, address Address) (Address, error)
 	// Delete deletes the specified address for the specified customer
 	Delete(id int64, addressID int64) error
 }
