26eebdfc43865c416ff66773d4a54dd6005c338e
Ianmohc <ian@mohc.net>
2022-10-27 10:02:38 +0100

    refactor: some changes to make the tags consistent

--- tags.go
+++ tags.go
@@ -7,33 +7,26 @@ type Tags string
 
 // NewTags combines a list of tags into a single shopify tag string
 func NewTags(tags []string) Tags {
-	return Tags(strings.Join(tags, ", "))
+	return Tags(strings.Join(tags, ","))
 }
 
 // Split parses the tag string and returns a list of the individual tags
 func (tags Tags) Split() []string {
-	splitStrings := strings.Split(string(tags), ", ")
+	splitStrings := strings.Split(string(tags), ",")
 	if len(splitStrings) == 1 && splitStrings[0] == "" {
 		return []string{}
+	}
+
+	for i := range splitStrings {
+		splitStrings[i] = strings.TrimSpace(splitStrings[i])
 	}
 
 	return splitStrings
 }
 
-// GetTagValue returns the value of a tag based on the key provided. The separator character is used to separate the key from value key:value
-func GetTagValue(tags []string, key string, separator string) string {
-	for _, tag := range tags {
-		if strings.Contains(tag, key) {
-			return strings.Split(tag, separator)[1]
-		}
-	}
-
-	return ""
-}
-
 // Add adds a new tag at the beginning of the comma-separated tag list
 func (tags Tags) Add(newTag string) Tags {
-	return Tags(strings.Join(append(tags.Split(), newTag), ", "))
+	return Tags(strings.Join(append(tags.Split(), newTag), ","))
 }
 
 // RemoveByKey removes the first item found from the tag list that contains the specified key
@@ -49,4 +42,15 @@ func (tags Tags) RemoveByKey(key string) Tags {
 	}
 
 	return NewTags(tagsList)
+}
+
+// GetTagValue returns the value of a tag based on the key provided. The separator character is used to separate the key from value key:value
+func GetTagValue(tags []string, key string, separator string) string {
+	for _, tag := range tags {
+		if strings.Contains(tag, key) {
+			return strings.Split(tag, separator)[1]
+		}
+	}
+
+	return ""
 }
