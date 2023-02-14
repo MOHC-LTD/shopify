02e96a4da9ab534383a3e1f3c981060a140346df
Ianmohc <ian@mohc.net>
2023-02-14 09:57:06 +0000

    test: making a little clearer

--- line-items_test.go
+++ line-items_test.go
@@ -5,14 +5,13 @@ import "testing"
 // TestLineItem_GetPropertyByName tests that you can get an line item order property by its name
 func TestLineItem_GetPropertyByName(t *testing.T) {
 	lineItem := LineItem{}
-	property := struct {
-		Name  string
-		Value string
-	}{Name: "TestProp", Value: "TestValue"}
+	property := Property{Name: "TestProp", Value: "TestValue"}
 	lineItem.Properties = append(lineItem.Properties, property)
 
+	expectedValue := "TestValue"
+
 	prop := lineItem.GetPropertyByName("TestProp")
-	if prop.Value != "TestValue" {
-		t.Errorf("Was expecting: %v got %v", "TestProp", prop.Value)
+	if prop.Value != expectedValue {
+		t.Errorf("Was expecting: %v got %v", expectedValue, prop.Value)
 	}
 }
