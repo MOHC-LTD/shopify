c6e52bdac79852c740485610d324f4fe616b98c1
Ianmohc <ian@mohc.net>
2023-02-20 15:27:13 +0000

    feat: adding constants for shopify inventory policies

---
+++ inventory-constants.go
@@ -0,0 +1,7 @@
+package shopify
+
+// InventoryPolicyContinue is an inventory policy to indicate that customers ARE allowed to place orders for a product variant if it's out of stock.
+const InventoryPolicyContinue = "continue"
+
+// InventoryPolicyDeny is an inventory policy to indicate that customers are NOT allowed to place orders for a product variant if it's out of stock.
+const InventoryPolicyDeny = "deny"
