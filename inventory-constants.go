f83084fa90eebbd745b5edceb1ba63e2b31a0188
Ianmohc <ian@mohc.net>
2023-02-21 10:46:49 +0000

    refactor: updating these names

--- inventory-constants.go
+++ inventory-constants.go
@@ -1,7 +1,7 @@
 package shopify
 
-// InventoryPolicyContinue is an inventory policy to indicate that customers ARE allowed to place orders for a product variant if it's out of stock.
-const InventoryPolicyContinue = "continue"
+// InventoryPolicyAllowWhenOutOfStock is an inventory policy to indicate that customers ARE allowed to place orders for a product variant if it's out of stock.
+const InventoryPolicyAllowWhenOutOfStock = "continue"
 
-// InventoryPolicyDeny is an inventory policy to indicate that customers are NOT allowed to place orders for a product variant if it's out of stock.
-const InventoryPolicyDeny = "deny"
+// InventoryPolicyDenyWhenOutOfStock is an inventory policy to indicate that customers are NOT allowed to place orders for a product variant if it's out of stock.
+const InventoryPolicyDenyWhenOutOfStock = "deny"
