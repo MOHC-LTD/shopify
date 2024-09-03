0a143f9818c5ccc81fa0e248a74482956940119d
Rohan Leach <rohan.leach@lloydsonlinedoctor.com>
2024-09-03 13:09:29 +0100

    fix: add OrderStatusURL to the struct

--- orders.go
+++ orders.go
@@ -75,6 +75,8 @@ type Order struct {
 	NoteAttributes NoteAttributes
 	// OrderNumber tells you that this is the shop's nth order.
 	OrderNumber int
+	// orderStatusURL to find the order status
+	OrderStatusURL string
 	// PresentmentCurrency is the presentment currency that was used to display prices to the customer.
 	PresentmentCurrency string
 	// ProcessedAt is the date and time the order was created at
