0c626a128199b66edb0322833ab9907b477b6f8a
Nick Harper <nicholas.harper@mohc.net>
2025-01-30 10:03:54 +0000

    feat: check for attribute in note attributes

--- orders.go
+++ orders.go
@@ -198,3 +198,12 @@ type OrderRepository interface {
 	// UpdateMetafield updates a single metafield
 	UpdateMetafield(orderID int64, metafield Metafield) (Metafield, error)
 }
+
+func (noteAttributes NoteAttributes) noteAttributeExistInList(attribute NoteAttribute) bool {
+	for _, current := range noteAttributes {
+		if current.Name == attribute.Name && current.Value == attribute.Value {
+			return true
+		}
+	}
+	return false
+}
