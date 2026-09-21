0908330c0001993e33a8fa0492dfeb4260f02b4b
JamesFowke <117648048+JamesFowke@users.noreply.github.com>
2026-09-21 15:10:13 +0100

    feat: add taxable field to productVariant

--- variants.go
+++ variants.go
@@ -95,6 +95,8 @@ type Variant struct {
 	Price string
 	// CompareAtPrice is the price of the product variant before an adjustment or a sale.
 	CompareAtPrice string
+	// Taxable indicates whether the product variant is subject to sales tax.
+	Taxable bool
 	// ProductID is the unique numeric identifier for the product.
 	ProductID int64
 	// Barcode is the barcode, UPC, or ISBN number for the product.
